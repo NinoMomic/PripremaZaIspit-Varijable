aad05ace78b2018bb2a069a5dee47511e0f74b82
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-01-18 08:51:41 +0100

    Add files via upload

---
+++ PripremZaProvjeruZnanja1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PripremZaProvjeruZnanja1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3.2, b = 2.6, c = 7.8, d = 5.9;
+            double x = a*2+b*2+c*2+d*2/(a+b)*2+(c+d)*2;
+
+            Console.WriteLine("Vrijednost x je: " + x);
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ PripremaZaIspitZnanja3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PripremaZaIspitZnanja3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Molimo Vas da upišete neku rečenicu: ");
+            string s1 = Console.ReadLine();
+            string[] s2 = s1.Split();
+            Console.WriteLine(s2.First()+""+s2.Last());
+
+            Console.ReadKey();  
+        }
+    }
+}
